e96700d0ebb821721dc682f7f9bad645d530fef6
Dmitri D <bllester@mail.ru>
2018-04-05 00:53:06 +0300

    Add TextQuest project.

---
+++ TextQuest/Assets/Scripts/QuestController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class QuestList
+{
+	public string roomName;
+	[Multiline]
+	//[TextArea(3,10)]
+	public string text;
+	public Buttons[] buttons;
+}
+
+[System.Serializable]
+public class Buttons {
+	public string action;
+	public string text;
+}
+
+public class QuestController : MonoBehaviour {
+	
+	[Header("General settings")]
+	public GameObject buttonPrefab;
+	public GameObject buttonParent;
+
+	public string currentRoom;
+	[Space(10)]
+
+	[Header("Rooms settings")]
+	public QuestList[] questList;
+
+
+	private GameObject panelBtn;
+	private GameObject button;
+
+	Dictionary<string, int> states;
+
+	void Awake () {
+		states = new Dictionary<string, int> ();
+
+		for (int i = 0; i < questList.Length; i++) {
+			if (i == 0) {
+				currentRoom = questList [i].roomName;
+			}
+			states [questList [i].roomName] = i;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		SetActions (currentRoom);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void SetActions(string room) {
+		int roomId = states [room];	
+		for (int i=0; i<questList[roomId].buttons.Length; i++) {
+			float tempY = 40f * i;
+			string text = questList [roomId].buttons [i].text;
+			Debug.Log (text);
+			Transform tmpTransform = buttonParent.transform;
+			button = Instantiate (buttonPrefab, tmpTransform) as GameObject;
+			button.transform.Find ("Text").GetComponent<Text> ().text = text;
+			// [ left - bottom ]
+			button.GetComponent<RectTransform>().offsetMin = new Vector2(0f, (-15f - tempY));
+			// [ right - top ]
+			button.GetComponent<RectTransform>().offsetMax = new Vector2(0f, (15f - tempY));
+			button.transform.SetParent(buttonParent.transform, false);
+		}
+	}
+}
---
+++ TextQuest/Assets/Scripts/StatesLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatesLogic : MonoBehaviour {
+
+	enum QuestStates {Intro, Room, Window, Door};
+
+	public Text text;
+
+	private QuestStates currentStates;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (currentStates) {
+		case QuestStates.Intro:
+			IntroStateUpdate ();
+			break;
+		}
+	}
+
+	private void IntroStateUpdate() {
+		
+	}
+}
